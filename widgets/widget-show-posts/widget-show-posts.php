21eab2e276cea64eb6974101e1436ee0dfd0b223
madan <madan_limbu@hotmail.co.uk>
2018-06-12 15:35:01 +0100

    implemented custom widget to get post type, usually used in homepage

---
+++ widgets/widget-show-posts/widget-show-posts.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Widget Name: Widget Show Posts
+ * Description: Widget to create full page image
+ * Version: 0.2
+ * Author: Madan
+ */
+class Widget_Show_Posts extends SiteOrigin_Widget{
+    function __construct() {
+        $form_options = array(
+            'posts' => array(
+                'type' => 'posts',
+                'label' => __('Show some of the posts from the posts type', 'widget-form-fields-text-domain'),
+            )
+        );
+
+        parent::__construct(
+            'show-posts-widget',
+            __('Show posts in the widget', 'widget-form-fields-text-domain'),
+            array(
+                'description' => __('Show posts in the widgets', 'widget-form-fields-text-domain'),
+            ),
+            array(),
+            $form_options,
+            plugin_dir_path(__FILE__)
+        );
+    }
+
+    function get_template_name($instance) {
+        return 'show-posts-template';
+    }
+    function get_template_dir($instance) {
+        return 'show-posts-templates';
+    }
+}
+siteorigin_widget_register('show-posts-widget', __FILE__, 'Widget_Show_Posts');
