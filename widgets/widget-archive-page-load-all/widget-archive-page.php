9f511bcf839d120892529d6b374612688565c493
madan <madan_limbu@hotmail.co.uk>
2018-06-13 09:42:04 +0100

    template for arichive page with load all options

--- widgets/widget-archive-page-load-all/widget-archive-page.php
+++ widgets/widget-archive-page-load-all/widget-archive-page.php
@@ -1,11 +1,11 @@
 <?php
 /*
- * Widget Name: Widget Archive Page
- * Description: Widget to create full page image
+ * Widget Name: Widget Archive Page Load All
+ * Description: Widget to create full page image Load All
  * Version: 0.2
  * Author: Madan
  */
-class Widget_Archive_Page extends SiteOrigin_Widget{
+class Widget_Archive_Page_Load_All extends SiteOrigin_Widget{
     function __construct() {
         $form_options = array(
             'posts' => array(
@@ -30,10 +30,10 @@ class Widget_Archive_Page extends SiteOrigin_Widget{
         );
 
         parent::__construct(
-            'archive-page-widget',
-            __('Show Archive page from a posts in the widget', 'widget-form-fields-text-domain'),
+            'archive-page-load-all-widget',
+            __('Show Archive page from a posts in the widget Load all', 'widget-form-fields-text-domain'),
             array(
-                'description' => __('Show Archive Page from a post', 'widget-form-fields-text-domain'),
+                'description' => __('Show Archive Page from a post Load all', 'widget-form-fields-text-domain'),
             ),
             array(),
             $form_options,
@@ -42,10 +42,10 @@ class Widget_Archive_Page extends SiteOrigin_Widget{
     }
 
     function get_template_name($instance) {
-        return 'archive-page-template';
+        return 'archive-page-load-all-template';
     }
     function get_template_dir($instance) {
-        return 'archive-page-templates';
+        return 'archive-page-load-all-templates';
     }
 }
-siteorigin_widget_register('archive-page-widget', __FILE__, 'Widget_Archive_Page');
+siteorigin_widget_register('archive-page-load-all-widget', __FILE__, 'Widget_Archive_Page_Load_All');
